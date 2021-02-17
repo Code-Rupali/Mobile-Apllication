5e36e242d60d9386c4671d406f886ec8634e869f
rupali-04 <rupali-04>
2021-02-17 11:36:51 +0530

    first

---
+++ app.js
@@ -0,0 +1,278 @@
+// Book Class: Represents a Book
+// Book Class: Represents a Book
+class Mobile {
+  constructor(name, brand, price,color,ram,rom) {
+  this.name = name;
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+    this.ram = ram;
+    this.rom = rom;
+  
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayMobiles() {
+    const mobiles = Store.getMobiles();
+      mobiles.forEach((mobile) => UI.addMobileToList(mobile));
+    
+  }
+
+  static addMobileToList(mobile) {
+    const list = document.querySelector('#mobilelist');
+
+    const row = document.createElement('tr');
+row.classList.add(`${mobile.brand}`)
+    row.innerHTML = `
+     <td>${mobile.name}</td> 
+      <td>${mobile.brand}</td>
+      <td>${mobile.price}</td>
+       <td>${mobile.color}</td>
+      <td>${mobile.ram}</td>
+      <td>${mobile.rom}</td>
+     
+      <td><a href="#" class="close">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+
+  static deleteMobile(el) {
+    if(el.classList.contains('delete')) {
+   
+        el.parentElement.parentElement.remove();
+      
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#mobile_form');
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 1000);
+  }
+
+  static clearFields() {
+    document.querySelector('#name').value = '';
+    document.querySelector('#brand').value = '';
+    document.querySelector('#price').value = '';
+    document.querySelector('#color').value = '';
+    document.querySelector('#ram').value = '';
+    document.querySelector('#rom').value = '';
+    
+  }
+}
+
+// Store Class: Handles Local Storage
+class Store {
+  static getMobiles() {
+    let mobiles;
+    if(localStorage.getItem('mobiles') === null) {
+      mobiles = [];
+    } else {
+      mobiles = JSON.parse(localStorage.getItem('mobiles'));
+    }
+
+    return mobiles;
+  }
+
+  static addMobile(mobile) {
+    const mobiles = Store.getMobiles();
+    mobiles.push(mobile);
+
+    localStorage.setItem('mobiles', JSON.stringify(mobiles));
+  }
+
+  static removeMobile(name) {
+    const mobiles = Store.getMobiles();
+
+    mobiles.forEach((mobile, index) => {
+      if(mobile.name === name) {
+        mobiles.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('mobiles', JSON.stringify(mobiles));
+  }
+}
+
+// Event: Display Books
+
+
+// Event: Add a Book
+document.querySelector('#mobile_form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const name = document.querySelector('#name').value;
+  const brand = document.querySelector('#brand').value;
+  const price = document.querySelector('#price').value;
+const color = document.querySelector('#color').value;
+  const ram = document.querySelector('#ram').value;
+  const rom = document.querySelector('#rom').value;
+ 
+  // Validate
+  if(name === '' || brand === '' || price === '' || color === '' || ram === '' || rom === '') {
+    UI.showAlert('Please fill in all fields','danger');
+  } else {
+    // Instatiate book
+    const mobile = new Mobile(name,brand,price,color,ram,rom);
+
+    // Add Book to UI
+    UI.addMobileToList(mobile);
+
+    // Add book to store
+    Store.addMobile(mobile);
+
+    // Show success message
+    UI.showAlert('Mobile Added', 'success');
+
+    // Clear fields
+  
+   UI.clearFields();
+   
+  
+  
+  }
+});
+ document.addEventListener('DOMContentLoaded', UI.displayMobiles('All'));
+// Event: Remove a Book
+document.querySelector('#mobilelist').addEventListener('click', (e) => {
+  // Remove book from UI
+  UI.deleteMobile(e.target);
+
+  // Remove book from store
+  
+  Store.removeMobile(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert('Mobile Removed','danger');
+});
+
+
+
+// display setting....
+
+const header = document.querySelector('header'); 
+
+const addbtn = document.querySelector('.additeams')
+const mobileiteams = document.querySelector('form');
+const iteams = document.querySelector('table');
+addbtn.addEventListener('click',()=>{
+    mobileiteams.classList.add('ho');
+    header.classList.add('inactive');
+    iteams.classList.remove('show');
+});
+const close = document.querySelector('.close').addEventListener('click',()=>{
+     mobileiteams.classList.remove('ho');
+     header.classList.remove('inactive');
+})
+
+const displaybtn = document.querySelector('.displayiteams');
+
+displaybtn.addEventListener('click',()=>{
+mobileiteams.classList.remove('ho');
+    header.classList.add('inactive');
+    iteams.classList.add('show');
+})
+
+
+const filterMobiles = document.querySelector('.filterMobiles');
+filterMobiles.addEventListener('click',(e)=>{
+ const mobiles = document.querySelectorAll('tbody td');
+  const m = document.querySelectorAll('tbody tr');
+ console.log(mobiles);
+switch(e.target.value){
+  case 'all':
+   mobiles.forEach((mobile)=> {
+   mobile.style.display = 'flex';}
+ )
+    break;
+    case 'nokia':
+    mobiles.forEach((mobile)=> {
+      if(mobile.innerText == 'nokia'){
+            mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+   }
+ )
+break;
+case 'iphone':
+   mobiles.forEach((mobile) =>{
+      if(mobile.innerText == 'iphone'){
+            mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+  }
+  
+ );
+
+break;
+case 'oneplus':
+   mobiles.forEach((mobile) =>{
+   if(mobile.innerText == 'oneplus'){
+            mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+  }
+  
+ );
+
+break;
+
+case 'low':
+  mobiles.forEach((mobile) =>{
+      if(parseInt(mobile) >= 1000 && parseInt(mobile) <10000){
+   mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+  }
+  
+ );
+
+break;
+case 'med':
+      mobiles.forEach((mobile) =>{
+      if(parseInt(mobile) >= 25000 && parseInt(mobile) <50000){
+   mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+  }
+  
+ );
+
+break;
+
+case 'high':
+      mobiles.forEach((mobile) =>{
+      if(parseInt(mobile) >=50000){
+   mobile.style.display = 'flex';      
+      }
+      else{
+        mobile.style.display = 'none'; 
+      }
+  }
+  
+ );
+
+break;
+}})
+
